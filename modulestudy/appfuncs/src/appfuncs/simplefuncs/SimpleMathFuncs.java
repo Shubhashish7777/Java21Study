8058b507492aa313aae91cd29d139caf6769db80
Shubhashish Saha <shubhashish.saha.work@gmail.com>
2024-12-01 03:04:53 +0530

    module study

---
+++ modulestudy/appfuncs/src/appfuncs/simplefuncs/SimpleMathFuncs.java
@@ -0,0 +1,12 @@
+package appfuncs.simplefuncs;
+
+public class SimpleMathFuncs {
+
+    public static boolean isEven(int n){
+        return n%2==0;
+    }
+
+    public static boolean isFactor(int a, int b){
+        return a%b ==0;
+    }
+}
---
+++ modulestudy/appstart/src/appstart/myappmoddemo/MyModAppDemo.java
@@ -0,0 +1,8 @@
+package appstart.myappmoddemo;
+import appfuncs.simplefuncs.SimpleMathFuncs;
+public class MyModAppDemo {
+
+    public static void main(String [] args){
+        System.out.println(SimpleMathFuncs.isEven(5));
+    }
+}
